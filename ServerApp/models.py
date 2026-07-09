dd4b5c7b49c32692ec517a461f2ed97c2d69c884
Readwanul Hoq <readwanulh@gmail.com>
2026-07-09 11:19:30 +0600

    First Model

--- ServerApp/models.py
+++ ServerApp/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-# Create your models here.
-
 class Student(models.Model):
     name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    email = models.EmailField()
     address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 
